15ed848ca06fc8b41a9bad77a303f4eeaaec8d99
XaverHeuser <xaver.heuser@gmail.com>
2026-03-27 09:08:43 +0100

    Setup docker and postgres

--- src/ingestion/ingestion.py
+++ src/ingestion/ingestion.py
@@ -1,3 +1,24 @@
-def process_ingestion(folder_path):
-    """This function ..."""
-    pass
+import psycopg
+
+conn = psycopg.connect(
+    host="127.0.0.1",
+    port=5433,
+    dbname="task_db",
+    user="admin",
+    password="admin123!"
+)
+
+cur = conn.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        email TEXT,
+        name TEXT,
+        address TEXT
+    )
+""")
+
+conn.commit()
+cur.close()
+conn.close()
